ed811f161fec99f81882c12dedf42cbee4318e9d
moorthy21sparkout <moorthy@sparkouttech.com>
2024-09-15 23:13:36 +0530

    Angular-Routing-with-Child-Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,23 @@ import { HomeComponent } from './home/home.component';
 import { authGuard } from './auth.guard';
 import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent, canActivate: [authGuard] },
-    { path: 'login', component: LoginComponent },
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'projects/:id', component: AppComponent }
+    // { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+    // { path: 'login', component: LoginComponent },
+    // { path: '', redirectTo: 'login', pathMatch: 'full' },
+    // { path: 'projects/:id', component: AppComponent }
+
+    {
+        path: 'dashboard',
+        component: DashboardComponent,
+        children: [
+          { path: 'profile', component: ProfileComponent },
+          { path: 'settings', component: AboutComponent }
+        ]
+      },
+      { path: '', redirectTo: '/dashboard/profile', pathMatch: 'full' }
 ];
